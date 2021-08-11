ae9c00f027b0ec308db63c39c58be565414c8d32
sayantan1413 <sayantanbose2001@gmail.com>
2021-08-12 00:31:07 +0530

    First draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Navbar } from './Components';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Pages/HomePage/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <FooterContainer>
+                <FooterSubscription>
+                    <FooterSubheading>
+                        Join our exclusive
+                    </FooterSubheading>
+                </FooterSubscription>
+
+            </FooterContainer>
+        </div>
+    )
+}
+
+export default Footer
